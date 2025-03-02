38ce236e983cf06bed994fc880f2bd7a603b760f
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2025-03-02 22:29:22 +0330

    Update admin.py

--- needy/admin.py
+++ needy/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Needy
 
-
-admin.site.register(Needy)
+@admin.register(Needy)
+class NeedyAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "national_code", "phone_number", "path", "created_by", "birth_date")
+    list_filter = ("path", "marital_status", "is_covered", "religion")
+    search_fields = ("full_name", "national_code", "phone_number")
+    ordering = ("full_name",)
+    list_editable = ("path",)
+    list_per_page = 20
