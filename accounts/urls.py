ad0299bf1f8a8e3fcc4e51f5878fc7bca6679c16
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2025-03-02 23:02:30 +0330

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
+from accounts.views import login_view
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,16 @@ from django.contrib import messages
 
 
 def login_view(request):
+    print("++++++++++++++++++++++++++")
     if request.user.is_authenticated:
+        print("++++++++++++++++++++++++")
         return redirect('add_needy')
 
     message = None
     if request.method == 'POST':
-        phone_number = request.POST.get('phone_number')
-        password = request.POST.get('password')
+        print("+++++++++++++")
+        phone_number = request.POST.get('phone_number', '').strip()
+        password = request.POST.get('password', '').strip()
         user = authenticate(request, username=phone_number, password=password)
         if user is not None:
             login(request, user)
