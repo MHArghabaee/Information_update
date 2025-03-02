5b08e7dbce12c946b345b15d8873ecf223e833c7
MHArghabaee <mh.arghabaee@gmail.com>
2025-03-02 20:38:40 +0330

    Your commit message here

--- needy/urls.py
+++ needy/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_needy/', views.add_needy, name='add_needy'),
+    path('add/', views.add_needy, name='add_needy'),
 ]
