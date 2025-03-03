04adffa70aa326ead9c5b3b72e2926d309556331
MHArghabaee <mh.arghabaee@gmail.com>
2025-03-03 21:12:55 +0330

    update

--- needy/urls.py
+++ needy/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('success/', views.success_view, name='success_view'),
     path('list/', views.needy_list, name='needy_list'),
     path('needy/delete/<int:id>/', views.delete_needy, name='delete_needy'),
-    path('export_needy/', views.export_needy_to_excel, name='export_needy_to_excel'),
+    path('export/', views.export_needy_to_excel, name='export_needy_to_excel'),
 ]
