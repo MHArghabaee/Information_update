77433a0c40917bf52a5d71cc6e4e29d58b91deb0
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2025-03-05 15:30:50 +0330

    Redirect the user to the list page after login and delete extra prints

--- accounts/views.py
+++ accounts/views.py
@@ -4,20 +4,17 @@ from django.contrib import messages
 
 
 def login_view(request):
-    print("++++++++++++++++++++++++++")
     if request.user.is_authenticated:
-        print("++++++++++++++++++++++++")
-        return redirect('add_needy')
+        return redirect('needy_list')
 
     message = None
     if request.method == 'POST':
-        print("+++++++++++++")
         phone_number = request.POST.get('phone_number', '').strip()
         password = request.POST.get('password', '').strip()
         user = authenticate(request, username=phone_number, password=password)
         if user is not None:
             login(request, user)
-            return redirect('add_needy')
+            return redirect('needy_list')
         else:
             message = "نام کاربری یا رمز عبور اشتباه است."
     return render(request, 'accounts/login.html', {'message': message})
